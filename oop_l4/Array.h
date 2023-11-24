757e3fb9364a2913d5a990452ecd9b0059ca8edd
TatianaBas <145323065+TatianaBas@users.noreply.github.com>
2023-11-24 18:10:01 +0300

    Update Array.h

--- oop_l4/Array.h
+++ oop_l4/Array.h
@@ -7,33 +7,146 @@
 template <class T>
 class Array{
 public:
-    Array();
+    Array(){
+        _capable = 10;
+        _size = 0;
+        _array = std::shared_ptr<T[]>(new T[_capable]);
 
-    Array(int size);
+        for (int i = 0; i < _capable; ++i)
+            _array[i] = nullptr;
+    }       
 
-    Array(const std::initializer_list<T> &t);
+    Array(int size){
+        _capable = size * 2;
+        _size = 0;
+        _array = std::shared_ptr<T[]>(new T[_capable]);
 
-    Array(const Array &other);
+        for (int i = 0; i != _capable; ++i)
+            _array[i] = nullptr;
+    }   
 
-    Array(Array &&other);
+    Array(const std::initializer_list<T> &t){
+            _size = t.size();
+        _capable = _size * 2;
+        _array = std::shared_ptr<T[]>(new T[_capable]);
+        int i = 0;
+        for (auto &c : t)
+            _array[i++] = c;
+    
+        for (int i = _size; i < _capable; ++i){
+            _array[i++] = nullptr;
+    }
+    }
 
-    ~Array();
+    Array(const Array &other){
+        _capable = other._capable;
+        _size = other._size;   
+        _array = std::shared_ptr<T[]>(new T[_capable]);
 
-    T& operator[](int index);
+        for (int i = 0; i < _size; ++i)
+            _array[i] = other._array[i];
+    }
 
-    Array<T>& operator=(Array<T>& other);
+    Array(Array &&other){
+        _capable = other._capable;
+        _size = other._size;
+        _array = other._array;
 
-    Array<T>& operator=(Array<T>&& other);
+        other._capable = 0;
+        other._size = 0;
+        other._array = nullptr;
+    }
 
-    bool operator==(Array<T>&other);
+    ~Array(){
+        _size = 0;
+        _capable = 0;
+    }
 
-    int size() const;
+    T& operator[](int index){
+        if (index >= _size)
+            throw std::invalid_argument("The array index is out of range");
+        return _array[index];
+    }
 
-    void delete_elem(int index);
+    Array<T>& operator=(Array<T>& other){
+        _capable = other._capable;
+        _size = other._size;
+        for(int i = 0; i < _size; ++i){
+            _array[i] = other._array[i];
+        }
+        for(int i{_size}; i < _capable; ++i){
+            _array[i] = nullptr;
+        }
 
-    void push_back(const T& elem);
+        return *this;
+    }
 
-    double sum() const;
+    Array<T>& operator=(Array<T>&& other){
+        _capable = other._capable;
+        _size = other._size;
+        for(int i = 0; i < _size; ++i){
+            _array[i] = other._array[i];
+        }
+        for(int i{_size}; i < _capable; ++i){
+            _array[i] = nullptr;
+        }
+
+        other.~Array();
+
+        return *this;
+    }
+
+    bool operator==(Array<T>&other){
+        if(_size == other._size){
+            for(int i = 0; i < _size; ++i){
+                if(_array[i] != other._array[i]){
+                    return false;
+                }
+            }
+            return true;
+        }
+        return false;
+    }
+
+    int size() const{
+        return _size;
+    }
+
+    void delete_elem(int index){
+        for(int i{index}; i < _size - 1; ++i){
+            _array[i] = _array[i + 1];
+        }
+        _array[_size - 1] = nullptr;
+        --_size;
+    }
+
+    void push_back(const T& elem){
+        if(_size == _capable){
+            _capable *= 2;
+            std::shared_ptr<T[]> tmp = std::shared_ptr<T[]>(new T[_capable]);
+            for(int i = 0; i < _size; ++i){
+                tmp[i] = _array[i];
+            }
+            for(int i{_size}; i < _capable; ++i){
+                tmp[i] = nullptr;
+            }
+            _array = tmp;
+            _array[_size++] = elem;
+        }
+        else{
+            _array[_size++] = elem;
+        }   
+    }
+
+    double sum() const{
+        double s = 0.0;
+
+        for(int i = 0; i < _size; ++i){
+            s += (double)(*_array[i]);
+        }
+
+        return s;
+    }
 
 private:
     int _size;
@@ -41,158 +154,4 @@ private:
     std::shared_ptr<T[]> _array;
 };
 
-template <class T>
-inline Array<T>::Array(){
-    _capable = 10;
-    _size = 0;
-    _array = std::shared_ptr<T[]>(new T[_capable]);
-
-    for (int i = 0; i < _capable; ++i)
-        _array[i] = nullptr;
-}
-
-template <class T>
-inline Array<T>::Array(int size) {
-    _capable = size * 2;
-    _size = 0;
-    _array = std::shared_ptr<T[]>(new T[_capable]);
-
-    for (int i = 0; i != _capable; ++i)
-        _array[i] = nullptr;
-}
-
-template <class T>
-inline Array<T>::Array(const std::initializer_list<T> &t){
-    _size = t.size();
-    _capable = _size * 2;
-    _array = std::shared_ptr<T[]>(new T[_capable]);
-    int i = 0;
-    for (auto &c : t)
-        _array[i++] = c;
-    
-    for (int i{_size}; i < _capable; ++i){
-        _array[i++] = nullptr;
-    }
-}
-
-template <class T>
-inline Array<T>::Array(const Array &other){
-    _capable = other._capable;
-    _size = other._size;   
-    _array = std::shared_ptr<T[]>(new T[_capable]);
-
-    for (int i = 0; i < _size; ++i)
-        _array[i] = other._array[i];
-}
-
-template <class T>
-inline Array<T>::Array(Array &&other){
-    _capable = other._capable;
-    _size = other._size;
-    _array = other._array;
-
-    other._capable = 0;
-    other._size = 0;
-    other._array = nullptr;
-}
-
-template <class T>
-inline T& Array<T>::operator[](int index){
-    if (index >= _size)
-        throw std::invalid_argument("The array index is out of range");
-    return _array[index];
-}
-
-template <class T>
-inline Array<T>& Array<T>::operator=(Array<T>& other){
-    _capable = other._capable;
-    _size = other._size;
-    for(int i = 0; i < _size; ++i){
-        _array[i] = other._array[i];
-    }
-    for(int i{_size}; i < _capable; ++i){
-        _array[i] = nullptr;
-    }
-
-    return *this;
-}
-
-template <class T>
-inline Array<T>& Array<T>::operator=(Array<T>&& other){
-    _capable = other._capable;
-    _size = other._size;
-    for(int i = 0; i < _size; ++i){
-        _array[i] = other._array[i];
-    }
-    for(int i{_size}; i < _capable; ++i){
-        _array[i] = nullptr;
-    }
-
-    other.~Array();
-
-    return *this;
-}
-
-template <class T>
-inline bool Array<T>::operator==(Array<T>& other){
-    if(_size == other._size){
-        for(int i = 0; i < _size; ++i){
-            if(_array[i] != other._array[i]){
-                return false;
-            }
-        }
-        return true;
-    }
-    return false;
-}
-
-template <class T>
-inline int Array<T>::size() const{
-    return _size;
-}
-
-template <class T>
-inline void Array<T>::delete_elem(int index){
-    for(int i{index}; i < _size - 1; ++i){
-        _array[i] = _array[i + 1];
-    }
-    _array[_size - 1] = nullptr;
-    --_size;
-}
-
-template <class T>
-inline void Array<T>::push_back(const T& elem){
-    if(_size == _capable){
-        _capable *= 2;
-        std::shared_ptr<T[]> tmp = std::shared_ptr<T[]>(new T[_capable]);
-        for(int i = 0; i < _size; ++i){
-            tmp[i] = _array[i];
-        }
-        for(int i{_size}; i < _capable; ++i){
-            tmp[i] = nullptr;
-        }
-        _array = tmp;
-        _array[_size++] = elem;
-    }
-    else{
-        _array[_size++] = elem;
-    }
-}
-
-template <class T>
-inline double Array<T>::sum() const{
-    double s = 0.0;
-
-    for(int i = 0; i < _size; ++i){
-        sm += (double)(*_array[i]);
-    }
-
-    return sm;
-}
-
-template <class T>
-inline Array<T>::~Array(){
-    _size = 0;
-    _capable = 0;
-}
 
